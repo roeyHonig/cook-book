7b4c9e9761ceab069438ab86128cb8e24346740f
roeyHonig <roeyhonig94@gmail.com>
2018-06-20 12:44:39 +0300

    learned how to work with CGRect & Heigh NSLayout Constraints

--- cook-book/RecipiesViewController.swift
+++ cook-book/RecipiesViewController.swift
@@ -10,19 +10,34 @@
 import UIKit
 
 class RecipiesViewController: UIViewController {
-    @IBOutlet weak var recta: UIView!
+    @IBOutlet weak var rec1: UIView!
+    @IBOutlet weak var rec2: UIView!
+    @IBOutlet weak var rec3: UIView!
+    @IBOutlet weak var rec1H: NSLayoutConstraint!
+    @IBOutlet weak var rec2H: NSLayoutConstraint!
+    @IBOutlet weak var rec3H: NSLayoutConstraint!
+    
+    
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        guard let width = recta.superview?.frame.size.width , let _ = recta.superview?.frame.size.height  else {return}
+        //guard let width = recta.superview?.frame.size.width , let _ = recta.superview?.frame.size.height  else {return}
+        //let origPoint = 0.5 * width
+        //recta.frame.origin.x = origPoint
+        //guard let height = header.superview?.frame.size.height else {print("problem with superView?");return}
+        //header.frame.size.height = 0.1 * height
+        //print("The Hegot of superView: \(height)")
+       // typeHeader.frame.size.height = 0.1 * height
+        //recipiesCollection.frame.size.height = 0.8 * height
         
-        let origPoint = 0.5 * width
-        recta.frame.origin.x = origPoint
-        
+        rec1H.constant = 0.1 * rec1.superview!.frame.size.height
+        rec2H.constant = 0.1 * rec1.superview!.frame.size.height
+        rec3H.constant = 0.8 * rec1.superview!.frame.size.height
        
-        
     }
 
     override func didReceiveMemoryWarning() {
