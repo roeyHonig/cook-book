c99302bfa0311877772916fe7356c84b875e1a38
roeyHonig <roeyhonig94@gmail.com>
2018-07-01 17:10:15 +0300

    1sr commit

--- cook-book/AppDelegate.swift
+++ cook-book/AppDelegate.swift
@@ -4,7 +4,7 @@
 //
 //  Created by hackeru on 3 Tamuz 5778.
 //  Copyright © 5778 student.roey.honig. All rights reserved.
-// lets bring the cool page
+// lets bring the cool pullUp Controller
 
 import UIKit
 import CoreData
