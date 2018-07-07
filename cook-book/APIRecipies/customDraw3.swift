fb98ff8e952c0c8b4c55fb78b85ffd555af03907
roeyHonig <roeyhonig94@gmail.com>
2018-07-07 13:48:54 +0300

    custom drawing views

---
+++ cook-book/APIRecipies/customDraw3.swift
@@ -0,0 +1,34 @@
+//
+//  customDraw3.swift
+//  cook-book
+//
+//  Created by hackeru on 24 Tamuz 5778.
+//  Copyright © 5778 student.roey.honig. All rights reserved.
+//
+
+import UIKit
+@IBDesignable
+class customDraw3: UIView {
+
+    override func draw(_ rect: CGRect) {
+        let path = UIBezierPath()
+        path.lineWidth = 4
+        UIColor.black.setStroke()
+        path.move(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX * 0.5, y: rect.maxY))
+        path.stroke()
+        path.lineWidth = 2
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.minY))
+        path.stroke()
+    }
+    
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ cook-book/APIRecipies/customeDraw4.swift
@@ -0,0 +1,32 @@
+//
+//  customeDraw4.swift
+//  cook-book
+//
+//  Created by hackeru on 24 Tamuz 5778.
+//  Copyright © 5778 student.roey.honig. All rights reserved.
+//
+
+import UIKit
+@IBDesignable
+class customeDraw4: UIView {
+
+    
+    override func draw(_ rect: CGRect) {
+        let path = UIBezierPath()
+        path.lineWidth = 4
+        UIColor.black.setStroke()
+        path.move(to: CGPoint(x: rect.minX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))
+        path.stroke()
+        
+    }
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
