4a244a7e3898e429a988597207ae17d5e50d58a6
roeyHonig <roeyhonig94@gmail.com>
2018-07-08 09:37:06 +0300

    added shopping cart icon to the shoppingList itemBar

--- cook-book/APIRecipies/RecipeDetailsViewController.swift
+++ cook-book/APIRecipies/RecipeDetailsViewController.swift
@@ -4,7 +4,7 @@
 //
 //  Created by hackeru on 8 Tamuz 5778.
 //  Copyright © 5778 student.roey.honig. All rights reserved.
-//
+//  TODO: just finsih the last icons and text in the recipe header
 
 import UIKit
 import SDWebImage
