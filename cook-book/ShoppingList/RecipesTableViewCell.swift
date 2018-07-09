173a25f8711a26658a543bbd97a1f02dc8b2629d
roeyHonig <roeyhonig94@gmail.com>
2018-07-09 16:52:43 +0300

    short and sweet programmatically adding a subview into the main Table cell

--- cook-book/ShoppingList/RecipesTableViewCell.swift
+++ cook-book/ShoppingList/RecipesTableViewCell.swift
@@ -28,6 +28,26 @@ class RecipesTableViewCell: UITableViewCell {
         // Configure the view for the selected state
     }
     
+    func showSecondaryTable()  {
+        let myFrame = CGRect(origin: self.insideTableContainer.frame.origin, size: CGSize(width: self.label.frame.width/* - cell.label.layoutMargins.left - cell.label.layoutMargins.right*/, height: 50))
+        let testView = UIView(frame: myFrame)
+        testView.backgroundColor = UIColor.blue
+        
+        //cell.insideTableContainer.addSubview(testView)
+        self.contentView.addSubview(testView)
+        
+        // constraints
+        let tableViewConstraintTop = NSLayoutConstraint(item: testView, attribute: NSLayoutAttribute.top, relatedBy: .equal, toItem: self.label, attribute: NSLayoutAttribute.bottom , multiplier: 1, constant: 0)
+        let tableViewConstraintBottom = NSLayoutConstraint(item: testView, attribute: NSLayoutAttribute.bottom, relatedBy: .equal, toItem: self.bottomSpacer, attribute: NSLayoutAttribute.top , multiplier: 1, constant: 0)
+        let tableViewConstraintLeft = NSLayoutConstraint(item: testView, attribute: NSLayoutAttribute.trailing, relatedBy: .equal, toItem: self.label, attribute: NSLayoutAttribute.trailing , multiplier: 1, constant: 0)
+        let tableViewConstraintRight = NSLayoutConstraint(item: testView, attribute: NSLayoutAttribute.leading, relatedBy: .equal, toItem: self.label, attribute: NSLayoutAttribute.leading , multiplier: 1, constant: 0)
+        
+        // assign the constraint to a coummon annssector
+        self.contentView.addConstraint(tableViewConstraintTop)
+        self.contentView.addConstraint(tableViewConstraintBottom)
+        self.contentView.addConstraint(tableViewConstraintLeft)
+        self.contentView.addConstraint(tableViewConstraintRight)
+    }
    
 
 }
