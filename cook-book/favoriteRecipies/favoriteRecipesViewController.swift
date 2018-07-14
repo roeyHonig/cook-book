1a6d0756926d37644bbf603c1f45847ec1007359
roeyHonig <roeyhonig94@gmail.com>
2018-07-14 03:05:19 +0300

    perfect, simply perfect, click to toggle the cross line, with safe keeping to make sure when the animation has ended

--- cook-book/favoriteRecipies/favoriteRecipesViewController.swift
+++ cook-book/favoriteRecipies/favoriteRecipesViewController.swift
@@ -9,9 +9,11 @@
 import UIKit
 import SDWebImage
 
-class favoriteRecipesViewController: UIViewController {
+class favoriteRecipesViewController: UIViewController, CAAnimationDelegate {
     
-    
+    var isDrawingNow = false
+    let layer = CAShapeLayer()
+    var toCross = true
     @IBOutlet var animatedView: UIView!
     
     override func viewDidLoad() {
@@ -19,48 +21,102 @@ class favoriteRecipesViewController: UIViewController {
     
         // Do any additional setup after loading the view.
         
-       
-        
-    }
-
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
         // prepre the path
-        let path = UIBezierPath()
-        let rect = animatedView.frame
-        
-        // cross line
-        path.move(to: CGPoint(x: rect.minX, y: rect.midY))
-        path.addLine(to: CGPoint(x: rect.maxX, y: rect.midY))
+       
         
         
         // Create shape layer and add the path to it
-        let layer = CAShapeLayer()
+        
+        
+        
+        let tapGesture = UITapGestureRecognizer()
+        tapGesture.addTarget(self, action: #selector(toggleCrossStatus))
+        animatedView.addGestureRecognizer(tapGesture)
+        
+    }
+
+    @objc func toggleCrossStatus(){
+        if !self.isDrawingNow{
+            self.animateCrossPath()
+        }
+        
+    }
+    
+    func animateCrossPath() {
+        self.isDrawingNow = true
+        // prepre the path
+        let path = UIBezierPath()
+        
+    
+        // draw the line from right to left
+        let rect = animatedView.frame
+        let newx = rect.midX
+        let newy = rect.midY
+        
+        let origx = newx - ((rect.size.width * 0.8) / 2)
+        let origy = newy - ((rect.size.height * 0.8) / 2)
+        let cgpoint = CGPoint(x: origx, y: origy)
+        let newSize = CGSize(width: rect.size.width * 0.8, height: rect.size.height * 0.8)
+        let rec = CGRect(origin: cgpoint, size: newSize)
+        
+        path.move(to: CGPoint(x: rec.minX , y: rec.midY))
+        path.addLine(to: CGPoint(x: rec.maxX, y: rec.midY))
+        
         layer.path = path.cgPath
         
         // Set up the appearance of the shape layer
-        layer.strokeEnd = 0 // in animation it will change to 1
+        
         layer.lineWidth = 10
+        if toCross {
+             layer.strokeEnd = 0 // in animation it will change to 1
+        } else {
+             layer.strokeEnd = 1 // in animation it will change to 0
+        }
         layer.strokeColor = UIColor.black.cgColor
+        layer.lineCap = kCALineCapRound
         //layer.fillColor = UIColor.clear.cgColor
+        
+        
+        
+        // Create the animation for the shape view
+        let animation = CABasicAnimation(keyPath: "strokeEnd")
+        animation.delegate = self
+        if toCross {
+            animation.toValue = 1
+            animation.fillMode = kCAFillModeForwards
+            animation.isRemovedOnCompletion = false
+        } else {
+            animation.toValue = 0
+            animation.fillMode = kCAFillModeForwards
+            animation.isRemovedOnCompletion = false
+        }
+        
+        animation.duration = 6 // seconds
+        animation.autoreverses = false
+        
+        
+        animation.timingFunction = CAMediaTimingFunction(name: "easeOut")
+        
         
         // Add the shape layer to view
         animatedView.layer.addSublayer(layer)
         
-        // Create the animation for the shape view
-        let animation = CABasicAnimation(keyPath: "strokeEnd")
-        animation.toValue = 1
-        animation.duration = 2 // seconds
-        animation.autoreverses = false
-        animation.fillMode = kCAFillModeForwards
-        animation.isRemovedOnCompletion = false
-        animation.timingFunction = CAMediaTimingFunction(name: "easeOut")
-        
-        
         // And finally add the linear animation to the shape!
         layer.add(animation, forKey: "line")
+       
+        
     }
     
+    
+    func animationDidStop(_ anim: CAAnimation, finished flag: Bool) {
+        print("Now, Now The animation has ended")
+        if self.toCross {
+            self.toCross = false
+        } else {
+            self.toCross = true
+        }
+        self.isDrawingNow = false
+    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
