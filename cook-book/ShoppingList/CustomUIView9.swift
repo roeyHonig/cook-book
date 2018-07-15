4a842a53b6ed213787a13ec86436b2aad1518825
roeyHonig <roeyhonig94@gmail.com>
2018-07-15 21:47:22 +0300

    customView

--- cook-book/ShoppingList/CustomUIView9.swift
+++ cook-book/ShoppingList/CustomUIView9.swift
@@ -7,8 +7,40 @@
 //
 
 import UIKit
+import CoreData
 
 class CustomUIView9: UIView {
+    
+    var numOfVerticalSections: Int
+    var currentNumOfVerticalSections: Int
+    var parentUIView: UIView
+    
+    init(currentNumOfVerticalSections i: Int, outOfTotalNumOfVerticalSections num: Int, toBeSubViewdIn view: UIView ) {
+        currentNumOfVerticalSections = i
+        numOfVerticalSections = num
+        parentUIView = view
+        super.init(frame: parentUIView.frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func draw(_ rect: CGRect) {
+        guard numOfVerticalSections > 0 else {
+            return
+        }
+       
+        let path = UIBezierPath()
+        path.lineWidth = 5
+        path.lineCapStyle = CGLineCap.round
+        UIColor.purple.setStroke()
+        let rec = CGRect(x: rect.minX, y: CGFloat(Float(currentNumOfVerticalSections - 1)) * (rect.height / CGFloat(Float(numOfVerticalSections))), width: rect.width, height: rect.height / CGFloat(Float(numOfVerticalSections)))
+        path.move(to: CGPoint(x: rec.minX + 5, y: rec.midY))
+        path.addLine(to: CGPoint(x: rec.maxX - 5, y: rec.midY))
+        path.stroke()
+        
+    }
 
     /*
     // Only override draw() if you perform custom drawing.
