59a825c6a2b7b168245bbbd1ea89a1dbdaa9a285
roeyHonig <roeyhonig94@gmail.com>
2018-07-15 21:42:03 +0300

    trying a new approach of array of UIVIews

---
+++ cook-book/ShoppingList/CustomUIView9.swift
@@ -0,0 +1,21 @@
+//
+//  CustomUIView9.swift
+//  cook-book
+//
+//  Created by hackeru on 3 Av 5778.
+//  Copyright © 5778 student.roey.honig. All rights reserved.
+//
+
+import UIKit
+
+class CustomUIView9: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
