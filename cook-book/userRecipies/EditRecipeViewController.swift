6da0a3629c70beace84a29a2e678d2bedaa9e1e7
roeyHonig <roeyhonig94@gmail.com>
2018-08-03 14:00:21 +0300

    we've finish checking valid \ invalid characters

--- cook-book/userRecipies/EditRecipeViewController.swift
+++ cook-book/userRecipies/EditRecipeViewController.swift
@@ -114,55 +114,20 @@ class EditRecipeViewController: UIViewController, UITableViewDelegate, UITableVi
         var invalidCharctersFound = false
         for i in 0...(theCurrentDataSource.count - 1) {
             for j in 0...(theCurrentDataSource[i].count - 1) {
-                if theCurrentDataSource[i][j].contains("[") || theCurrentDataSource[i][j].contains("]") || theCurrentDataSource[i][j].contains("{") || theCurrentDataSource[i][j].contains("}") || theCurrentDataSource[i][j].contains("\"" + ",") {
-                    invalidCharctersFound = true
-                    
-                }
-                
-                //for uniCodeScalr in theCurrentDataSource[i][j].
-                
-                    for v in theCurrentDataSource[i][j].unicodeScalars {
-                        //print(v.value)
-                        if v.value == 8220 || v.value == 8221 {
+                        if theCurrentDataSource[i][j].contains("[") || theCurrentDataSource[i][j].contains("]") || theCurrentDataSource[i][j].contains("{") || theCurrentDataSource[i][j].contains("}") {
                             invalidCharctersFound = true
                         }
-                    }
-                    
-                    /*
-                    for c in theCurrentDataSource[i][j].characters {
-                        
-                        if c.unicodeScalars.first?.value == 8220 {
-                            print("double qutation found")
-                            //invalidCharctersFound = true
-                        } else  {
-                            print(c)
+                
+                        for v in theCurrentDataSource[i][j].unicodeScalars {
+                            //print(v.value)
+                            if v.value == 8220 || v.value == 8221 || v.value == 8216 || v.value == 8217 {
+                                invalidCharctersFound = true
+                            }
                         }
- 
-                        
-                     
-                        
-                        //print("\(c.unicodeScalars.first?.value)")
-                        /*
-                         if c == Character(UnicodeScalar(8220)) {
-                         print("fucked")
-                         } else {
-                         print("\(c.unicodeScalars.first?.value)")
-                         
-                         //UnicodeScalar(34)?.value
-                         }
-                         */
-                        
-                    }
- */
-                }
-                
-                
-                
-            
+            }
+     
         }
-        /*theCurrentDataSource[i][j].contains("""
-         "
-         """) */
+        
         if invalidCharctersFound {
             print("invalid charcter found")
             return
