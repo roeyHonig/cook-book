1d5df795cff21424142f4831dac1005a100b9583
roeyHonig <roeyhonig94@gmail.com>
2018-08-05 13:18:51 +0300

    partially hooked up

---
+++ cook-book/userRecipies/ImagePickerTutorialViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ImagePickerTutorialViewController.swift
+//  cook-book
+//
+//  Created by hackeru on 24 Av 5778.
+//  Copyright © 5778 student.roey.honig. All rights reserved.
+//
+
+import UIKit
+
+class ImagePickerTutorialViewController: UIViewController, UIImagePickerControllerDelegate , UINavigationControllerDelegate{
+
+    @IBOutlet var imageView: UIImageView!
+    @IBAction func pickAnImageFromUsrLibary(_ sender: UIButton) {
+        // TOOD: Make this work
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true) {
+            // compleation code
+        }
+    }
+    let imagePicker = UIImagePickerController()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+        imagePicker.delegate = self
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
